20dba6ef546cb4d8c0fd26aaef96188819a8e7c3
sergei_vokhminov <rreaper@list.ru>
2024-08-20 22:42:32 +0300

    обновлен модуль category

--- src/category.py
+++ src/category.py
@@ -15,26 +15,24 @@ class Category:
         self.description = description
         self.products = products if products else []
         Category.category_count += 1
-        Category.product_count += len(products)  # if products else 0
+        Category.product_count += len(products) if products else 0
 
 
 if __name__ == "__main__":
     products = Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     products1 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    products2 = Product("перец", "перец для пикника", 23.6, 1)
+    products2 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
 
-    user_category = Category("овощи", "для супа", [products, products1, products2])
-    user_category1 = Category("овощи", "для супа", [])
+    user_category = Category("Смартфоны", "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни", [products, products1])
+    user_category1 = Category("Телевизоры", "Фоновая подсветка", [products2])
 
     print(user_category.name)
     print(user_category.description)
-    print(user_category.products_list)
-    print()
-    print(user_category.category_count)
-    print(user_category.product_count)
+    print(user_category.products)
     print()
     print(user_category1.name)
     print(user_category1.description)
-    print(user_category1.products_list)
-    print(user_category1.category_count)
-    print(user_category1.product_count)
+    print(user_category1.products)
+    print()
+    print(user_category.category_count)
+    print(user_category.product_count)
