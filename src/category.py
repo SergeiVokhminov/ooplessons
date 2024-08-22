6928e26e8b24cff3beefa8910b9c830a54600926
sergei_vokhminov <rreaper@list.ru>
2024-08-22 21:18:04 +0300

    обновлены модули category.py, main.py, product.py

--- src/category.py
+++ src/category.py
@@ -13,37 +13,66 @@ class Category:
     def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        #  Сделали атрибут приватным.
+        self.__products = products if products else []
         Category.category_count += 1
-        Category.product_count += len(products) if products else 0
+        Category.product_count += len(self.__products)
 
-    def add_product(cls, ):
-        pass
+    #  Создаем атрибут как геттер (используя декоратор). Позволяет обращаться к методу, как атрибуту класса.
+    #  Данный атрибут будет возвращать строку.
+    @property
+    def products(self):
+        """Вывод списка товаров из приватного атрибута."""
+        products_str = ""
+        for product in self.__products:
+            products_str += f"{product.name} {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_str
+
+    def add_product(self, new_product: Product):
+        """Добавление товаров и запись в приватный атрибут списка товаров."""
+        for product in self.__products:
+            if product.name == new_product.name:
+                product.quantity += new_product.quantity
+                if product.price <= new_product.price:
+                    product.price = new_product.price
+                return
+
+        else:
+            self.__products.append(new_product)
+            Category.product_count += 1
+            return self.__products
 
 
 if __name__ == "__main__":
-    products = Product("Samsung Galaxy C23 Ultra",
-                       "256GB, Серый цвет, 200MP камера",
-                       180000.0,
-                       5)
-    products1 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    products2 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    products1 = Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    products2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    products3 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
 
-    user_category = Category(
+    category1 = Category(
         "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, "
-        "но и получение дополнительных функций для удобства жизни",
-        [products, products1],
+        "Смартфоны, как средство не только коммуникации, " "но и получение дополнительных функций для удобства жизни",
+        [products1, products2],
     )
-    user_category1 = Category("Телевизоры", "Фоновая подсветка", [products2])
 
-    print(user_category.name)
-    print(user_category.description)
-    print(user_category.products)
+    print(category1.name)
+    print(category1.description)
+    print(category1.products)
+    print(category1.category_count)
+    print(category1.product_count)
     print()
-    print(user_category1.name)
-    print(user_category1.description)
-    print(user_category1.products)
+
+    category2 = Category("Телевизоры", "Фоновая подсветка", [products3])
+
+    print(category2.name)
+    print(category2.description)
+    print(category2.products)
+    print(category2.category_count)
+    print(category2.product_count)
     print()
-    print(user_category.category_count)
-    print(user_category.product_count)
+
+    products4 = Product('45" QLED', "Фоновая подсветка", 101000.0, 3)
+    category2.product = products4
+
+    print(category2.products)
+    print(category2.product_count)
+    print(category2.category_count)
--- src/product.py
+++ src/product.py
@@ -9,31 +9,59 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
-    # def new_product(self):
-    #     pass
+    @classmethod
+    def new_product(cls, new_product: dict):
+        name = new_product.get("name")
+        description = new_product.get("description")
+        price = new_product.get("price")
+        quantity = new_product.get("quantity")
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        elif new_price < self.__price:
+            user_answer = input(
+                "Подтвердите изменение цены!\n" "Для подтверждения введите: y\n" "Для отмены введите: n\n"
+            ).lower()
+            if user_answer == "y":
+                self.__price = new_price
+            elif user_answer == "n":
+                self.__price = self.__price
+        else:
+            self.__price = new_price
 
 
 if __name__ == "__main__":
-    user_prod = Product("Samsung Galaxy C23 Ultra",
-                        "256GB, Серый цвет, 200MP камера",
-                        180000.0,
-                        5)
-    user_prod1 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    user_prod2 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
-    print(user_prod.name)
-    print(user_prod.description)
-    print(user_prod.price)
-    print(user_prod.quantity)
+    prod1 = Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    prod2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    prod3 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+
+    print(prod1.name)
+    print(prod1.description)
+    print(prod1.price)
+    print(prod1.quantity)
     print()
-    print(user_prod1.name)
-    print(user_prod1.description)
-    print(user_prod1.price)
-    print(user_prod1.quantity)
+    print(prod2.name)
+    print(prod2.description)
+    print(prod2.price)
+    print(prod2.quantity)
     print()
-    print(user_prod2.name)
-    print(user_prod2.description)
-    print(user_prod2.price)
-    print(user_prod2.quantity)
+    print(prod3.name)
+    print(prod3.description)
+    print(prod3.price)
+    print(prod3.quantity)
+    print()
+    prod4 = Product.new_product({"name": '43" FullHD', "description": "Смарт ТВ", "price": 43000.0, "quantity": 5})
+    print(prod4.name)
+    print(prod4.description)
+    print(prod4.price)
+    print(prod4.quantity)
